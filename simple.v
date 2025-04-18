a2478dba6fcf0bfb8ca9ba3e4de143aeba981327
A-m-e-y <ameyk1510@gmail.com>
2025-04-17 23:42:02 -0700

    Remove obsolete files and directories related to the floating-point Q-value update implementation

--- simple.v
+++
@@ -1,3 +0,0 @@
-module simple(input wire a, output wire b);
-    assign b = a;
-endmodule
--- test_simple.py
+++
@@ -1,8 +0,0 @@
-import cocotb
-from cocotb.triggers import Timer
-
-@cocotb.test()
-async def test_simple(dut):
-    dut.a.value = 1
-    await Timer(1, units="ns")
-    assert dut.b.value == 1
